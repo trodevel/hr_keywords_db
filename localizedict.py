d8c5c41b32cd5cc57f7b2b194453887e8b1b6b4f
trodevel <trodevel@gmail.com>
2023-05-24 02:32:36 +0200

    added localizedict

---
+++ localizedict.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+'''
+localizedict.
+
+Copyright (C) 2023 Dr. Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
+from hr_keywords.python.languages import Language
+
+##########################################################
+
+class localizedict:
+
+    def __init__( self, lang: ):
+        pass
+
+    def __str__(self):
+        return ""
+
+    def __len__( self ):
+        return 0
+
+    def get( self, idd: int ):
+        pass
+
+##########################################################
