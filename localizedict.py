b1f4f5692721996902a9d38d93a14baee569d554
trodevel <trodevel@gmail.com>
2023-06-14 02:42:31 +0200

    First try (#1)
    * added localizedict
    
    * syntax error fix
    
    * intermediate results
    
    * syntax error fix
    
    * added _generate_filename()
    
    * intermediate results
    
    * added test
    
    * syntax error fix
    
    * intermediate results
    
    * syntax error fix
    
    * syntax error fix
    
    * syntax error fix
    
    * intermediate results
    
    * intermediate results
    
    * bugfix
    
    * added more tests
    
    * added .gitignore
    
    * added more tests
    
    * typo
    
    * bugfix
    
    * added throw_on_error
    
    * integrated throw_on_error
    
    * added more tests
    
    * added more tests

---
+++ localizedict.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+'''
+localizedict.
+
+Copyright (C) 2023 Dr. Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
+from hr_keywords.python.languages import Language
+import csv
+
+##########################################################
+
+class localizedict:
+
+    dictt: dict = None
+    throw_on_error: bool = None
+
+    def __init__( self, prefix: str, lang: Language, throw_on_error: bool = False ):
+        filename = localizedict._generate_filename( prefix, lang )
+        self._load( filename )
+        self.throw_on_error = throw_on_error
+
+    def __str__(self):
+        return str( self.dictt )
+
+    def __len__( self ):
+        return len( self.dictt )
+
+    def get( self, idd: int ):
+
+        if self.throw_on_error or ( idd in self.dictt ):
+            return self.dictt[ idd ]
+
+        return str( idd )
+
+    ##########################################################
+
+    def _generate_filename( prefix: str, lang: Language ) -> str:
+        res = f"{prefix}.{lang.name}.csv"
+        return res
+
+    def _load_elem_v_1( data: list, filename: str ) -> []:
+
+        if len( data ) < 2:
+            raise Exception( f"load_elem_v_1: broken record in {filename}: expected 2 or more fields, {len(data)} is given" )
+
+        key                 = int( data[0] )
+        val                 = data[1]
+
+        return key, val
+
+    def _load_v_1( csvfile, filename: str ) -> dict:
+
+        res = dict()
+
+        reader = csv.reader( csvfile, delimiter=';' )
+
+        for row in reader:
+
+            key, val = localizedict._load_elem_v_1( row, filename )
+            res[ key ] = val
+            #print( f"DEBUG: _load_v_1: key {key}, val {val}" )
+
+        print( "INFO: _load_v_1: read {} records from {} (v1)".format( len( res ), filename ) )
+
+        return res
+
+    def _load( self, filename: str ):
+
+        with open( filename ) as csvfile:
+            self.dictt = localizedict._load_v_1( csvfile, filename )
+
+        print( f"INFO: _load: read {len(self.dictt)} records from {filename}" )
+
+##########################################################
---
+++ localizedict_test.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python3
+
+'''
+localizedict test.
+
+Copyright (C) 2023 Dr. Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
+from hr_keywords.python.languages import Language
+
+import localizedict
+
+##########################################################
+
+gl_dict_01_en = None
+gl_dict_01_ru = None
+gl_dict_02_en = None
+gl_dict_02_ru = None
+gl_dict_03 = None
+
+##########################################################
+
+def create_dict_01_en():
+
+    global gl_dict_01_en
+
+    if not gl_dict_01_en:
+        gl_dict_01_en = localizedict.localizedict( "resources/locations", Language.en )
+
+    return gl_dict_01_en
+
+##########################################################
+
+def create_dict_01_ru():
+
+    global gl_dict_01_ru
+
+    if not gl_dict_01_ru:
+        gl_dict_01_ru = localizedict.localizedict( "resources/locations", Language.ru )
+
+    return gl_dict_01_ru
+
+##########################################################
+
+def create_dict_02_en():
+
+    global gl_dict_02_en
+
+    if not gl_dict_02_en:
+        gl_dict_02_en = localizedict.localizedict( "resources/specializations", Language.en )
+
+    return gl_dict_02_en
+
+##########################################################
+
+def create_dict_03():
+
+    global gl_dict_03
+
+    if not gl_dict_03:
+        gl_dict_03 = localizedict.localizedict( "resources/specializations", Language.en, True )
+
+    return gl_dict_03
+
+##########################################################
+
+def create_dict_02_ru():
+
+    global gl_dict_02_ru
+
+    if not gl_dict_02_ru:
+        gl_dict_02_ru = localizedict.localizedict( "resources/specializations", Language.ru )
+
+    return gl_dict_02_ru
+
+##########################################################
+
+def test_01():
+
+    d = create_dict_01_en()
+
+    idd = 2867714
+
+    word = d.get( idd )
+
+    print( f"test_01: id '{idd}', word = {word}" )
+
+##########################################################
+
+def test_02():
+
+    d = create_dict_01_ru()
+
+    idd = 2867714
+
+    word = d.get( idd )
+
+    print( f"test_02: id '{idd}', word = {word}" )
+
+##########################################################
+
+def test_03():
+
+    d = create_dict_02_en()
+
+    idd = 121
+
+    word = d.get( idd )
+
+    print( f"test_03: id '{idd}', word = {word}" )
+
+##########################################################
+
+def test_04():
+
+    d = create_dict_02_ru()
+
+    idd = 121
+
+    word = d.get( idd )
+
+    print( f"test_04: id '{idd}', word = {word}" )
+
+##########################################################
+
+def test_05():
+
+    d = create_dict_02_en()
+
+    idd = 10000
+
+    word = d.get( idd )
+
+    print( f"test_05: id '{idd}', word = {word}" )
+
+##########################################################
+
+def test_06():
+
+    d = create_dict_03()
+
+    idd = 10000
+
+    try:
+        word = d.get( idd )
+
+        print( f"test_06: id '{idd}', word = {word}" )
+
+    except Exception as e:
+
+        print( f"test_06: id '{idd}', got exception {e}" )
+
+##########################################################
+
+def test():
+
+    test_01()
+    test_02()
+    test_03()
+    test_04()
+    test_05()
+    test_06()
+
+##########################################################
+
+if __name__ == "__main__":
+   test()
+
+##########################################################
