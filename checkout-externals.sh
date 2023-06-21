99eabda802bd461af78cdc92008176ea7b5161e9
trodevel <trodevel@gmail.com>
2023-06-21 14:25:36 +0200

    refactor: adjust to hr_keywords repo split (#2)
    * tools: added checkout-externals.sh
    
    * updated .gitignore
    
    * adjusted to hr_keywords repo split
    
    * updated filenames
    
    * updated filenames

---
+++ checkout-externals.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+svn export https://github.com/trodevel/hr_locations/tags/1.0.0/resources externals/locations
+svn export https://github.com/trodevel/hr_specializations/tags/2.0.0/resources externals/specializations
