6c77db7796a4cb5892de16f3cd42e3b847cd0694
trodevel <trodevel@gmail.com>
2023-06-21 14:20:14 +0200

    adjusted to hr_keywords repo split

--- localizedict_test.py
+++ localizedict_test.py
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 '''
 
-from hr_keywords.python.languages import Language
+from languages.python.languages import Language
 
 import localizedict
 
